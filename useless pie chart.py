a97f3e4fca761f41c7f35370b49d98d46d95d8a5
Julian-Campione <julian.campione@gmail.com>
2025-10-27 01:44:45 -0400

    useless pie chart.py

--- useless pie chart.py
+++ useless pie chart.py
@@ -7,14 +7,14 @@ b["Date"] = pd.to_datetime(b["Date"])
 latestdate = b["Date"].max()
 #This part of the code is to change the date into a format that pd knows
 #max() looks at the largest number aka latest date 
-# --- 3. Filter the data for the latest date ---
-latest_row = b[b["Date"] == latestdate].iloc[0]
+# looking only for the first/latestdate
+latest-row = b[b["Date"] == latestdate].iloc[0]
 #b[b["Date"] == latestdate means to only look at the numebers from the latest date
 #iloc 0 means the first row and iloc is so that the data is not in a table (can't plot/do when in a table)
 
 #selecting the columns that are not the date colunm (actual data)
 maturity_columns = [col for col in b.columns if col != "Date"]
-rates = latest_row[maturity_columns].dropna()
+rates = latest-row[maturity_columns].dropna()
 
 #code for pie chart
 mplt.figure(figsize=(10,10))
